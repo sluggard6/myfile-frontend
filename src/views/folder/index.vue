f393e25543c18663b24b2bd5f773a247a50e6aba
吴方 <wufang@wufangdeMacBook-Pro.local>
2021-01-31 15:01:50 +0800

    update

--- src/views/folder/index.vue
+++ src/views/folder/index.vue
@@ -3,13 +3,40 @@
     <el-row>
       <el-col :span="24"><div class="grid-content bg-purple-dark">详情页面{{ folderId }}</div></el-col>
     </el-row>
-    <el-table :data="children" style="width: 100%" highlight-current-row>
+    <el-table :data="children" style="width: 100%" highlight-current-row @cell-mouse-enter="optionShow" @cell-mouse-leave="optionHide">
       <el-table-column type="selection" width="55" />
       <el-table-column label="名称" width="120">
         <template slot-scope="{row}">
-          <router-link :to="'/folder/1'">
+          <router-link v-if="row.isFolder" :to="'/folder/'+row.id">
             <el-link class="link-type">{{ row.name }}</el-link>
           </router-link>
+          <router-link v-else :to="'/file/'+row.id">
+            <el-link class="link-type">{{ row.name }}</el-link>
+          </router-link>
+        </template>
+      </el-table-column>
+      <el-table-column label="大小" width="120">
+        <template slot-scope="{row}">
+          <div v-if="row.isFolder">-</div>
+          <div v-else>{{ row.size }}</div>
+        </template>
+      </el-table-column>
+      <el-table-column prop="address" label="操作" show-overflow-tooltip>
+        <template slot-scope="{row}">
+          <transition name="el-fade-in">
+            <div v-show="row.display">
+              <el-tooltip effect="light" placement="top" content="分享" transition="el-fade-in">
+                <div class="icon-item">
+                  <i class="el-icon-share" @click="{dialogShow=true;title='共享资料库'+row.name}" />
+                </div>
+              </el-tooltip>
+              <el-tooltip effect="light" placement="top" content="删除" transition="el-fade-in">
+                <div class="icon-item">
+                  <i class="el-icon-delete-solid" />
+                </div>
+              </el-tooltip>
+            </div>
+          </transition>
         </template>
       </el-table-column>
     </el-table>
@@ -22,6 +49,7 @@
             :data="params"
             :on-preview="handlePreview"
             :on-remove="handleRemove"
+            :on-success="handleSuccess"
             :before-remove="beforeRemove"
             :with-credentials="withCredentials"
             :show-file-list="false"
@@ -36,6 +64,7 @@
 </template>
 <script>
 import { getChildren } from '@/api/folder'
+import { Message } from 'element-ui'
 
 export default {
   name: 'Folder',
@@ -64,8 +93,34 @@ export default {
       // this.folders = res.data.folders
       // this.files = res.data.files
       console.log(res)
-      this.children = res.data.folders.concat(res.data.files)
+      this.children = res.data.folders.map(function(obj, index) {
+        obj.isFolder = true
+        obj.show = false
+        return obj
+      // }).concat(res.data.files)
+      }).concat(res.data.files.map(function(obj, index) {
+        obj.isFolder = false
+        obj.show = false
+        return obj
+      }))
+      // this.children = res.data.folders.concat(res.data.files)
       console.log(this.children)
+    },
+    optionShow(row) {
+      row.display = true
+    },
+    optionHide(row) {
+      row.display = false
+    },
+    handleSuccess(res, file, fileList) {
+      console.log(res)
+      if (res.code !== 0) {
+        Message({
+          message: res.message || 'Error',
+          type: 'error',
+          duration: 5 * 1000
+        })
+      }
     },
     handleRemove() {
       console.log('---------handleRemove---------')
