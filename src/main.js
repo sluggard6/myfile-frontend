ca48d3064e91a739c5da0150085cf9778b027a4d
sluggard <sydra@163.com>
2021-03-24 15:15:37 +0800

    update

--- src/main.js
+++ src/main.js
@@ -15,6 +15,7 @@ import router from './router'
 import '@/icons' // icon
 import '@/permission' // permission control
 import myUpload from 'vue-image-crop-upload'
+import uploader from 'vue-simple-uploader'
 /**
  * If you don't want to use mock-server
  * you want to use MockJs for mock api
@@ -33,7 +34,7 @@ Vue.component('my-upload', myUpload)
 Vue.use(ElementUI, { locale })
 // 如果想要中文版 element-ui，按如下方式声明
 // Vue.use(ElementUI)
-
+Vue.use(uploader)
 Vue.config.productionTip = false
 
 new Vue({
